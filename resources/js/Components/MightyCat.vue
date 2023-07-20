e0732275b93a4e40844ea1b993dd23e365cf28b2
Omar Muñoz <dev@omarmunoz.tech>
2023-07-20 14:11:06 -0600

    feat: Implement mighty cat level up visualization

--- resources/js/Components/MightyCat.vue
+++ resources/js/Components/MightyCat.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { ref, watch } from 'vue'
   import { computedAsync } from '@vueuse/core'
   import { useUserNftModule } from '@/Modules/UserNftModule'
 
@@ -6,10 +7,16 @@
   import mightyCat1 from '@img/mighty-cat/1.png'
   import mightyCat2 from '@img/mighty-cat/2.png'
 
+  import bgLevelUp from '@img/mighty-cat/bg-level-up.png'
+
   const imgMap = {
     0: mightyCat0,
     1: mightyCat1,
     2: mightyCat2,
+  }
+
+  const options = {
+    levelUpStateDuration: 1500,
   }
 
   const userNftModule = useUserNftModule()
@@ -17,10 +24,33 @@
   const nftData = computedAsync(async () => {
     return await userNftModule.getUserNftData()
   }, null)
+
+  const showLevelUpState = ref(false)
+  const onLevelUp = () => {
+    showLevelUpState.value = true
+    setTimeout(() => {
+      showLevelUpState.value = false
+    }, options.levelUpStateDuration)
+  }
+
+  watch(nftData, (newData, oldData) => {
+    if (newData && oldData) {
+      if (newData.level > oldData.level) {
+        onLevelUp()
+      }
+    }
+  })
 </script>
 
 <template>
   <div v-if="nftData" class="mighty-cat">
+    <div
+      v-show="showLevelUpState"
+      class="level-up-state"
+      :style="`--bg-level-up: url(${bgLevelUp})`"
+    >
+      <h3 v-if="showLevelUpState" class="label-level-up">Level up!</h3>
+    </div>
     <img :src="imgMap[nftData.version]" />
   </div>
 </template>
@@ -29,5 +59,40 @@
   img {
     width: 500px;
     max-width: 100%;
+  }
+
+  @keyframes slide-up {
+    0% {
+      transform: translate(-50%, 100%);
+    }
+
+    80% {
+      transform: translate(-50%, -50%);
+    }
+
+    100% {
+      transform: translate(-50%, 0);
+    }
+  }
+
+  .level-up-state {
+    position: absolute;
+    inset: -2rem 0;
+
+    display: block;
+
+    background: var(--bg-level-up) center bottom no-repeat;
+    background-size: 100%;
+
+    .label-level-up {
+      position: absolute;
+      top: 0;
+      left: 50%;
+
+      margin-top: -1rem;
+
+      transform: translate(-50%, 0);
+      animation: 0.2s ease-in 0s 1 slide-up;
+    }
   }
 </style>
